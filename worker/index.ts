2a815f9de568c4f4f186b7f63097df8474d592b1
liujip0 <liujip2020@gmail.com>
2026-06-23 23:18:28 +0800

    add trpc

--- worker/index.ts
+++ worker/index.ts
@@ -1,9 +1,18 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { createContext } from "./context.ts";
+import { appRouter } from "./router.ts";
+
 export default {
   fetch(request) {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith("/api/")) {
-      return Response.json({ name: "Cloudflare" });
+      return fetchRequestHandler({
+        endpoint: "/api",
+        req: request,
+        router: appRouter,
+        createContext,
+      });
     }
 
     return new Response(null, { status: 404 });
