f73e53146457a7596c126e4d67b2a8c4caf80332
liujip0 <liujip2020@gmail.com>
2026-06-24 22:39:41 +0800

    write login logic

--- worker/auth/login.ts
+++ worker/auth/login.ts
@@ -1,9 +1,36 @@
 import zod from "zod";
 import { publicProcedure } from "../trpc.ts";
+import type { User } from "../../types/Users.ts";
+import { TRPCError } from "@trpc/server";
+import * as argon2 from "node-argon2";
+import { generateToken } from "./tokens.ts";
 
 export const login = publicProcedure
   .input(zod.object({ username: zod.string(), password: zod.string() }))
   .mutation(async ({ input, ctx }) => {
-    input;
-    ctx;
+    const { DB, JWT_SECRET } = ctx.env;
+
+    const unauthorizedError = new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Invalid username or password",
+    });
+
+    const user = await DB.prepare("SELECT * FROM Users WHERE username = ?")
+      .bind(input.username)
+      .first<User>();
+
+    if (!user) {
+      throw unauthorizedError;
+    }
+
+    const verify = await argon2.verify({
+      hash: user.password,
+      password: input.password,
+    });
+
+    if (!verify) {
+      throw unauthorizedError;
+    }
+
+    return generateToken(input.username, user.secret, JWT_SECRET);
   });
